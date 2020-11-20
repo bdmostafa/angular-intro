240186f8134aa4bd6cdd8af99d465b3d450a7b07
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-11-20 16:15:08 +0600

    decorators example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,22 @@
 import { Component } from '@angular/core';
 
+function log(target, name, descriptor) {
+  console.log(target, name, descriptor)
+  //store the original function in a variable
+  const original = descriptor.value;
+
+  // Do some manipulaors with descriptor.value
+  descriptor.value = function(...args) {
+    console.log("Args", args, " were passed in this function")
+    const result = original.apply(this, args);
+    console.log("the result of the function is ", result);
+    return result;
+  }
+
+  return descriptor;
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +24,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intro2angular';
+
+  constructor() {
+    console.log("The statement was generated thruogh constructor", this.testMethod(1, 2, 3));
+  }
+
+  @log
+  testMethod(x, y, z){
+    return x + y +z;
+  }
+
 }
